8d44f305d6239953dbea86ccb2860bdc0ff7290d
Hellen <105614757+hesllens@users.noreply.github.com>
2023-03-27 21:52:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const team1 = "FALLEN";
+const team2 = "TRYBE";
+let turn = team1;
+
+let mappool = ["Train", "Mirage", "Cache", "Inferno", "Cobblestone"];
+
+const turnText = document.querySelector("#turnText");
+
+turnText.innerText = `É a vez da equipe ${turn} Banir o mapa`;
+
+const maps = document.querySelectorAll(".card");
+
+function choosemap(event) {
+
+  if (turn == team1) {
+    turn = team2
+  } else {
+    turn = team1
+  }
+  turnText.innerText = `É a vez da equipe ${turn} Banir o mapa`;
+
+  event.currentTarget.classList.add("selected");
+
+  event.currentTarget.removeEventListener("click", choosemap);
+
+  event.currentTarget.querySelector(".accept").innerText = "BANIDO";
+
+  const clickedmap = event.currentTarget.querySelector(".map-name").innerText;
+
+  mappool = mappool.filter(map => map != clickedmap);
+
+  if (mappool.length == 1) {
+    console.log(mappool)
+    const decidedMap = document.querySelector(".card:not(.selected)");
+    decidedMap.classList.add("picked");
+    decidedMap.removeEventListener("click",choosemap);
+    decidedMap.classList.add("disable-hover");
+    turnText.innerText=`O mapa escolhido foi ${mappool[0]}`
+  }
+}
+
+for (let index = 0; index < maps.length; index++) {
+  maps[index].addEventListener("click", choosemap)
+}
